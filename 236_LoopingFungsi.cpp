2e44a7bee1a865dd806313d5feaad0dd2db8f0c0
akmaalsyh <taufansyah50@gmail.com>
2024-03-19 14:43:10 +0700

    first commit

---
+++ 236_LoopingFungsi.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+    (
+    int i;
+    string nama(5); 
+    )
+    
+    for(i = 0; i < 7; i++);(
+        cout << i << "." << "AKMAL" << endl;
+    )
+
+    for(i=0; i < 5; i++);(
+        cout <<"Masukkan nama =";
+        cin >> nama(i);
+    )
+
+    COUT << endl;
+    cout << "--------------------" << endl;
+    cout << "KUMPULAN NAMA-NAMA" << endl;
+
+    for(i=0; i<5; i++);(
+        cout << "Namanya adalah" << nama(i) << endl;
+    )
